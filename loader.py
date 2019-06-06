c6666eb7a320915e096748417219d61bb9d415f7
Theodor Fragner <theo@fragner.org>
2019-06-06 16:11:21 +0200

    add error handling

--- loader.py
+++ loader.py
@@ -12,6 +12,9 @@ from time import sleep
 from urllib import request
 from subprocess import Popen, PIPE
 
+url = 'http://wienerlinien.at/ogd_realtime/monitor?rbl=2167&sender=k34RXngkrRDzfT2x'
+bin = '/home/pi/bim31monitor_rpi0/a.out'
+
 # utility for running other programs
 def run(args):
   proc = Popen(args, stdout = PIPE)
@@ -20,22 +23,39 @@ def run(args):
 
 # main part
 def main():
-  # download & parse json file
-  raw = request.urlopen('http://wienerlinien.at/ogd_realtime/monitor?rbl=2167&sender=k34RXngkrRDzfT2x')
-  data = json.loads(raw.read().decode("utf-8"))
+  try:
+    # download & parse json file
+    raw = request.urlopen(url)
+    data = json.loads(raw.read().decode("utf-8"))['data']['monitors'][0]['lines'][0]
+  except:
+    # fill data with dummy content
+    data = {
+      'barrierFree': False,
+      'departures': {
+        'departure': [{'departureTime': {'countdown': 0}} for i in range(3)]
+      }
+    }
+    print('couldn\'t download {}.'.format(url))
+    print('are you connected to the internet?')
+
 
   # write content to data file
   with open('data', 'w') as f:
-    line_barrierfree = data['data']['monitors'][0]['lines'][0]['barrierFree']
+    line_barrierfree = data['barrierFree']
 
     for i in range(3):
-      f.write(str(data['data']['monitors'][0]['lines'][0]['departures']['departure'][i]['departureTime']['countdown']) + '\n')
-      f.write(str(data['data']['monitors'][0]['lines'][0]['departures']['departure'][i]['vehicle']['barrierFree']
-        if 'vehicle' in data['data']['monitors'][0]['lines'][0]['departures']['departure'][i]
+      f.write(str(data['departures']['departure'][i]['departureTime']['countdown']) + '\n')
+      f.write(str(data['departures']['departure'][i]['vehicle']['barrierFree']
+        if 'vehicle' in data['departures']['departure'][i]
         else line_barrierfree) + '\n')
   
   # print tram positions to screen
-  run(['/home/pi/bim31monitor_rpi0/a.out'])
+  try:
+    run([bin])
+  except:
+    print('couldn\'t find {}.'.format(bin))
+    print('have you compiled the binaries with \'make\'?')
+
 
 if __name__ == '__main__':
   while True:
